780f86948cd76d397822b98e6dcebfcdd300365e
Diego Reyes <diegorele13@gmail.com>
2022-04-13 12:48:59 -0500

    add: the icons necessary

---
+++ helpers/icon.js
@@ -0,0 +1,15 @@
+import {
+    mdiFacebook,
+    mdiGithub,
+    mdiInstagram,
+    mdiTwitter,
+    mdiWeb,
+} from "@mdi/js";
+
+export default {
+    facebook: mdiFacebook,
+    twitter: mdiTwitter,
+    github: mdiGithub,
+    instagram: mdiInstagram,
+    web: mdiWeb,
+};

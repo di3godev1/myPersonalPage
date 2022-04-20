f165cd790882dd72b8f3c33c57e69ec3b0af3433
Diego Reyes <diegorele13@gmail.com>
2022-04-19 21:07:46 -0500

    refact: the hero in desktop screen

--- components/home/Hero.vue
+++ components/home/Hero.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="w-full max-w-7xl mx-auto overflow-x-hidden">
+    <div class="w-full mx-auto overflow-x-hidden max-w-screen-2xl">
         <div class="w-full flex flex-col justify-center items-center">
             <!-- FIRST SECTION -->
             <div class="w-full flex justify-center items-center">
                 <!-- NAME SECTION -->
-                <div class="w-3/4 flex justify-center">
+                <div class="w-3/4 flex justify-center relative left-5 top-7">
                     <div
                         class="w-full flex flex-col justify-center items-center"
                     >
@@ -15,24 +15,25 @@
                                 <p
                                     class="text-xs font-secondary text-greyDiego"
                                 >
-                                    {{ h1 }}
+                                    {{ openTitle }}
                                 </p>
                             </div>
-                            <div class="w-full">
+                            <div class="w-full relative lg:left-36">
                                 <div class="">
                                     <p
-                                        class="uppercase text-whiteDiego lg:text-5xl"
+                                        class="uppercase text-whiteDiego lg:text-7xl"
                                     >
                                         hello
                                     </p>
                                 </div>
-                                <div class="">
+                                <div class="relative left-7 lg:text-7xl">
                                     <p class="uppercase text-whiteDiego">I'm</p>
                                 </div>
                             </div>
                         </div>
                         <div class="w-full flex flex-row justify-start">
-                            <div class="relative w-3/4">
+                            <!-- SVGNAMESECTIOM -->
+                            <div class="relative w-3/4 top-5 left-6">
                                 <my-name class="flex sm:hidden" />
                                 <my-name-tablet
                                     class="hidden sm:flex lg:hidden"
@@ -45,15 +46,15 @@
                 <!-- IMAGE SECTION -->
                 <div class="w-1/4">
                     <div class="w-full flex justify-center items-center">
-                        <div class="relative w-full bg-blackDiego">
+                        <div class="relative w-full bg-blackDiego lg:-top-20">
                             <div
-                                class="w-36 h-36 bg-pinkDiego rounded-full absolute top-0 -right-9"
+                                class="w-36 h-36 bg-pinkDiego rounded-full absolute -top-6 -right-9 lg:w-96 lg:h-96"
                             >
                                 <img
                                     src="https://scontent.fgye4-1.fna.fbcdn.net/v/t39.30808-6/246461857_579754813269051_2270695955322423465_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=19026a&_nc_eui2=AeGMACMPNU3oK0YKJVqslhMKyyiwZR7f0lrLKLBlHt_SWvOltMLOo4L6g5hV1yKPxYTyp6B2rpmijfXqdNjRgZGH&_nc_ohc=sHpNpAzHlUcAX-eWAlR&_nc_ht=scontent.fgye4-1.fna&oh=00_AT-BfKg_t3xvI0i8VIYqDcMOtuaT9JcLgZCpcxXHBjYXMw&oe=62616662"
                                     alt=""
                                     srcset=""
-                                    class="object-cover rounded-full w-36 h-36 border-t-2 border-l-4 border-b-8 border-pinkDiego"
+                                    class="object-cover rounded-full w-36 h-36 border-t-2 border-l-4 border-b-8 border-pinkDiego lg:w-96 lg:h-96"
                                 />
                             </div>
                         </div>
@@ -69,12 +70,16 @@
                         class="flex flex-col justify-center items-center h-28 w-1/3 my-0 mx-auto"
                     >
                         <div class="relative w-full">
-                            <p class="uppercase text-whiteDiego text-right">
+                            <p
+                                class="text-2xl uppercase text-whiteDiego text-right"
+                            >
                                 front-
                             </p>
                         </div>
                         <div class="relative w-full">
-                            <p class="uppercase text-whiteDiego text-right">
+                            <p
+                                class="text-2xl uppercase text-whiteDiego text-right"
+                            >
                                 end
                             </p>
                         </div>
@@ -83,17 +88,18 @@
                         class="bg-blackDiego relative w-1/3 -top-9 right-8 my-0 mx-auto"
                     >
                         <p
-                            class="uppercase text-whiteDiego relative left-full top-1"
+                            class="text-2xl uppercase text-whiteDiego relative left-full top-1"
                         >
                             developer
                         </p>
                         <div class="flex justify-end left-full relative top-1">
                             <p class="text-greyDiego justify-end text-xs">
-                                {{ closedH1 }}
+                                {{ closeTitle }}
                             </p>
                         </div>
                     </div>
                 </div>
+                <!-- DESCRIPTION SECTION -->
                 <div class="w-full h-52 flex justify-end">
                     <div class="w-1/3 h-52">
                         <p class="text-whiteDiego">
@@ -101,7 +107,8 @@
                         </p>
                     </div>
                 </div>
-                <div class="w-full h-auto relative -top-80 -left-7">
+                <!-- SECOND IMAGE SECTION -->
+                <div class="w-full relative -top-80 -left-7">
                     <div
                         class="w-56 h-56 rounded-full border-8 border-pinkDiego absolute -left-8"
                     >

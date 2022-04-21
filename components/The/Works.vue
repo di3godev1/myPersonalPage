ee89947ed502c06f75b2bcf21eafd913f31a2a81
Diego Reyes <diegorele13@gmail.com>
2022-04-21 01:03:16 -0500

    add: a way more scalable to put the works

---
+++ components/The/Works.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="w-full flex">
+        <div class="w-3/4 flex flex-row flex-wrap">
+            <div class="w-3/4">
+                <h3 class="text-2xl lg:text-4xl">{{ name }}</h3>
+                <p class="text-greyDiego text-xs sm:text-base">{{ date }}</p>
+            </div>
+            <div>
+                <p class="text-whiteDiego text-xs sm:text-base lg:text-2xl">
+                    {{ description }}
+                </p>
+            </div>
+            <div class="w-1/4">
+                <div class="w-9 h-9 rounded-full border border-pinkDiego"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        date: {
+            type: String,
+            required: true,
+        },
+    },
+};
+</script>

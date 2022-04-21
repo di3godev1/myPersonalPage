b3f72122d273cc700df7ccd86d40439e9550c032
Diego Reyes <diegorele13@gmail.com>
2022-04-20 20:35:39 -0500

    added: the svg that says work

--- components/home/Work.vue
+++ components/home/Work.vue
@@ -1,5 +1,22 @@
 <template>
     <section id="workSection">
-        <div></div>
+        <div class="w-full">
+            <my-work class="relative left-5 md:hidden lg:hidden" />
+            <my-work-desktop class="lg:flex hidden" />
+            <my-work-tablet class="md:flex hidden" />
+        </div>
     </section>
 </template>
+
+<script>
+import MyWork from "../global/MyWork.vue";
+import MyWorkDesktop from "../global/MyWork.vue";
+import MyWorkTablet from "../global/MyWork.vue";
+export default {
+    components: {
+        MyWork,
+        MyWorkDesktop,
+        MyWorkTablet,
+    },
+};
+</script>

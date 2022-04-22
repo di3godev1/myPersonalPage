6fbf0d8e07cc4f1a3c47c7a2982c3eea35799f80
Diego Reyes <diegorele13@gmail.com>
2022-04-21 21:50:28 -0500

    done: The svg in all the screens

--- components/home/Skills.vue
+++ components/home/Skills.vue
@@ -1,3 +1,21 @@
 <template>
-    <section id="skillsSection"></section>
+    <section id="skillsSection " class="pt-28">
+        <!-- SVG SECTION -->
+        <div class="w-full">
+            <div class="flex justify-center">
+                <!-- SVG TO MOBILE -->
+                <div>
+                    <my-skills class="flex sm:hidden" />
+                </div>
+                <!-- SVG TO DESKTOP -->
+                <div>
+                    <my-skills-desktop class="hidden lg:flex" />
+                </div>
+                <!-- SVG TO TABLET -->
+                <div>
+                    <my-skills-tablet class="hidden sm:flex lg:hidden" />
+                </div>
+            </div>
+        </div>
+    </section>
 </template>

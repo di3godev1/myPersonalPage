835befa8f90fcf618ee160302390a29ad289e594
Diego Reyes <diegorele13@gmail.com>
2022-04-21 19:15:13 -0500

    added: the work section

--- components/The/Works.vue
+++ components/The/Works.vue
@@ -1,19 +1,39 @@
 <template>
     <div class="w-full flex">
-        <div class="w-3/4 flex flex-row flex-wrap">
-            <div class="w-3/4">
-                <h3 class="text-2xl lg:text-4xl">{{ name }}</h3>
-                <p class="text-greyDiego text-xs sm:text-base">{{ date }}</p>
+        <!-- WORKS DETAILS SECTION -->
+        <div class="w-4/5">
+            <div class="w-full grid grid-cols-3 grid-rows-2">
+                <div class="">
+                    <h3 class="text-2xl lg:text-4xl">{{ name }}</h3>
+                </div>
+                <div class="row-start-2 col-start-1">
+                    <p class="text-greyDiego text-xs sm:text-base">
+                        {{ date }}
+                    </p>
+                </div>
+                <div class="row-start-3 col-end-4 col-start-1 relative -top-3">
+                    <p class="text-whiteDiego text-xs sm:text-base lg:text-2xl">
+                        {{ description }}
+                    </p>
+                </div>
+            </div>
+        </div>
+        <!-- <div class="w-3/4 flex flex-row flex-wrap">
+            <div class="w-3/4 flex flex-row">
+                <div>
+                    <h3 class="text-2xl lg:text-4xl">{{ name }}</h3>
+                    <p class="text-greyDiego text-xs sm:text-base">
+                        {{ date }}
+                    </p>
+                </div>
+                <div>hey</div>
             </div>
             <div>
                 <p class="text-whiteDiego text-xs sm:text-base lg:text-2xl">
                     {{ description }}
                 </p>
             </div>
-            <div class="w-1/4">
-                <div class="w-9 h-9 rounded-full border border-pinkDiego"></div>
-            </div>
-        </div>
+        </div> -->
     </div>
 </template>
 

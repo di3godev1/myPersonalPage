3f95d7bc101f36bb909fb454ff4ac2f3fc290683
Diego Reyes <diegorele13@gmail.com>
2022-05-08 17:25:37 -0500

    fix: the size of the circle in diferents screens

--- components/home/Skills.vue
+++ components/home/Skills.vue
@@ -19,7 +19,18 @@
         </div>
         <!-- DETAILS SKILLS SECTION   -->
         <div class="mt-16 w-full flex justify-center">
-            <div class="w-44 h-44 rounded-full border-pinkDiego border-2"></div>
+            <div
+                class="w-44 h-44 rounded-full border-pinkDiego border-2 border-l-8 border-y-2 border-r md:w-96 md:h-96 size-desktop"
+            ></div>
         </div>
     </section>
 </template>
+
+<style scoped>
+@media only screen and (min-device-width: 1024px) and (max-device-width: 1440px) {
+    .size-desktop {
+        width: 520px;
+        height: 520px;
+    }
+}
+</style>

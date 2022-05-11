b656edccb0b26d8b7f590a23a2f2f173278d419c
Diego Reyes <diegorele13@gmail.com>
2022-05-11 18:47:29 -0500

    add: the media component

--- components/home/SayHi.vue
+++ components/home/SayHi.vue
@@ -1,5 +1,5 @@
 <template>
-    <section id="say_hiSection" class="mt-20">
+    <section id="say_hiSection" class="mt-20 bg-blackDiego">
         <!-- SVG SECTION -->
         <div class="w-4/5 flex flex-col mx-auto max-w-7xl flex-wrap">
             <div class="flex justify-end pb-2 md:mr-10 lg:mr-1">
@@ -20,6 +20,8 @@
                 class="rounded-full w-64 h-64 border-l-8 border-r-2 border-t-4 border-b-4 border-pinkDiego md:w-96 md:h-96 relative -top-4 lg:left-12 lg:-top-10 md:-top-6"
             >
                 <HomeCardForm />
+            </div>
+            <div>
                 <Media />
             </div>
         </div>

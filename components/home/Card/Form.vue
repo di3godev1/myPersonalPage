f25e76952f1d44f4b05751ff2f1ff5dd8463fb4d
Diego Reyes <diegorele13@gmail.com>
2022-05-11 14:53:01 -0500

    add: best way to show the form

---
+++ components/home/Card/Form.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="w-full flex justify-center items-center">
+        <div class="flex flex-col">
+            <div class="">
+                <label for="name" class="text-whiteDiego"> name: </label>
+                <input
+                    type="text"
+                    placeholder="insert text"
+                    id="name"
+                    class="w-5/6 bg-blackDiego border-2 border-pinkDiego rounded-xl placeholder:text-sm placeholder:text-greyDiego placeholder:font-s"
+                />
+            </div>
+            <div class="">
+                <label for="email" class="text-whiteDiego"> email: </label>
+                <input
+                    type="text"
+                    placeholder="insert text"
+                    id="email"
+                    class="w-5/6 bg-blackDiego border-2 border-pinkDiego rounded-xl placeholder:text-sm placeholder:text-greyDiego placeholder:font-secondary"
+                />
+                <div>
+                    <input
+                        type="text"
+                        placeholder="insert text"
+                        class="w-5/6 bg-blackDiego border-2 border-pinkDiego rounded-xl placeholder:text-sm placeholder:text-greyDiego placeholder:font-secondary"
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>

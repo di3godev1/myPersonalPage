d082dc68d9141176aa4813e53160e43d61ff3153
Diego Reyes <diegorele13@gmail.com>
2022-05-11 18:47:58 -0500

    done: personal social media was rendered

--- components/home/Social/Media.vue
+++ components/home/Social/Media.vue
@@ -1,13 +1,16 @@
 <template>
     <div class="w-full">
-        <div class="w-4/5">
+        <div class="w-full flex flex-wrap justify-between mx-auto">
             <div
                 v-for="(icon, index) in icons"
                 :key="index"
-                class="w-12 h-12 border-l-2 border-x-4 border-r-8"
+                class="mt-14 ml-4 w-12 h-12 border-l-2 border-t-4 border-b-4 border-r-8 rounded-full border-pinkDiego flex justify-center items-center"
             >
                 <a :href="icon.url" target="_blank">
-                    <Icons class="w-6 h-6" :name="icon.name"></Icons>
+                    <Icons
+                        class="w-6 h-6 text-whiteDiego"
+                        :name="icon.name"
+                    ></Icons>
                 </a>
             </div>
         </div>
@@ -23,6 +26,18 @@ export default {
             {
                 name: "facebook",
                 url: "https://www.facebook.com/di3godev",
+            },
+            {
+                name: "instagram",
+                url: "https://www.instagram.com/di3godev/",
+            },
+            {
+                name: "twitter",
+                url: "https://twitter.com/di3godev",
+            },
+            {
+                name: "github",
+                url: "https://github.com/di3godev1",
             },
         ],
     }),

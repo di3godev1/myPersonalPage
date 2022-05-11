84c76fc17318477e6b81f5642ce264d8a70f8bec
Diego Reyes <diegorele13@gmail.com>
2022-05-11 15:46:45 -0500

    done: the form in mobilde view

--- components/home/Card/Form.vue
+++ components/home/Card/Form.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="w-full flex justify-center items-center">
-        <div class="flex flex-col">
-            <div class="">
+        <div class="flex flex-col relative top-7 left-4">
+            <div class="relative left-5">
                 <label for="name" class="text-whiteDiego"> name: </label>
                 <input
                     type="text"
                     placeholder="insert text"
                     id="name"
-                    class="w-5/6 bg-blackDiego border-2 border-pinkDiego rounded-xl placeholder:text-sm placeholder:text-greyDiego placeholder:font-s"
+                    class="w-2/4 bg-blackDiego border-2 border-pinkDiego rounded-3xl placeholder:text-sm placeholder:text-greyDiego placeholder:font-s"
                 />
             </div>
             <div class="">
@@ -16,14 +16,24 @@
                     type="text"
                     placeholder="insert text"
                     id="email"
-                    class="w-5/6 bg-blackDiego border-2 border-pinkDiego rounded-xl placeholder:text-sm placeholder:text-greyDiego placeholder:font-secondary"
+                    class="w-5/6 bg-blackDiego border-2 border-pinkDiego rounded-3xl placeholder:text-sm placeholder:text-greyDiego placeholder:font-secondary"
                 />
-                <div>
+                <div class="mt-2">
                     <input
                         type="text"
                         placeholder="insert text"
-                        class="w-5/6 bg-blackDiego border-2 border-pinkDiego rounded-xl placeholder:text-sm placeholder:text-greyDiego placeholder:font-secondary"
+                        class="w-5/6 h-16 bg-blackDiego border-2 border-pinkDiego rounded-3xl placeholder:text-sm placeholder:text-greyDiego placeholder:font-secondary"
                     />
+                </div>
+            </div>
+            <div class="w-full mt-3">
+                <div class="w-1/2 mx-auto relative right-5">
+                    <button
+                        type="submit"
+                        class="w-full text-whiteDiego border-2 border-pinkDiego rounded-3xl"
+                    >
+                        .send()
+                    </button>
                 </div>
             </div>
         </div>

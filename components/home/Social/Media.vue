77a61f4b6d879826a3049481acb10414d07ae5c7
Diego Reyes <diegorele13@gmail.com>
2022-05-11 18:07:07 -0500

    add:icon's component

---
+++ components/home/Social/Media.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="w-full">
+        <div class="w-4/5">
+            <div
+                v-for="(icon, index) in icons"
+                :key="index"
+                class="w-12 h-12 border-l-2 border-x-4 border-r-8"
+            >
+                <a :href="icon.url" target="_blank">
+                    <Icons class="w-6 h-6" :name="icon.name"></Icons>
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Icons from "../../global/Icons.vue";
+export default {
+    components: { Icons },
+    data: () => ({
+        icons: [
+            {
+                name: "facebook",
+                url: "https://www.facebook.com/di3godev",
+            },
+        ],
+    }),
+};
+</script>

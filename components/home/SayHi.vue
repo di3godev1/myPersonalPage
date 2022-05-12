34106bf1682927da63eae000d2fe7257aa544aa4
Diego Reyes <diegorele13@gmail.com>
2022-05-12 17:25:17 -0500

    fix: size of the form's circle

--- components/home/SayHi.vue
+++ components/home/SayHi.vue
@@ -5,7 +5,7 @@
             class="w-4/5 flex flex-col mx-auto max-w-7xl flex-wrap justify-center items-center"
         >
             <div
-                class="flex justify-end pb-2 md:mr-10 lg:mr-1 relative md:left-44 left-16"
+                class="flex justify-end pb-2 md:mr-10 lg:mr-1 relative md:left-40 left-16"
             >
                 <!-- SVG TO MOBILE -->
                 <div>
@@ -21,7 +21,7 @@
                 </div>
             </div>
             <div
-                class="rounded-full w-64 h-64 border-l-8 border-r-2 border-t-4 border-b-4 border-pinkDiego md:w-96 md:h-96 relative -top-4 lg:left-12 lg:-top-6 md:-top-6"
+                class="rounded-full w-64 h-64 border-l-8 border-r-2 border-t-4 border-b-4 border-pinkDiego md:w-96 md:h-96 relative -top-4 lg:left-12 lg:-top-6 md:-top-6 size-tablet"
             >
                 <HomeCardForm />
             </div>
@@ -40,3 +40,12 @@ export default {
     },
 };
 </script>
+
+<style scoped>
+@media only screen and (min-device-width: 768px) and (max-device-width: 1440px) {
+    .size-tablet {
+        width: 664px;
+        height: 664px;
+    }
+}
+</style>

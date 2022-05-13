665046fc7b1ad35596c29330bcb1f57ca651c39c
Diego Reyes <diegorele13@gmail.com>
2022-05-13 15:50:49 -0500

    add: section with the age and my name

--- components/home/Footer.vue
+++ components/home/Footer.vue
@@ -1,3 +1,28 @@
 <template>
-    <footer class="w-full h-12"></footer>
+    <footer class="w-full h-12 mt-5">
+        <div>
+            <div></div>
+            <div></div>
+        </div>
+        <div class="flex flex-wrap items-center">
+            <div class="mr-2">
+                <P class="text-whiteDiego">Diego Reyes</P>
+            </div>
+            <Icons class="w-3 h-3 mr-2 text-whiteDiego" :name="copy"></Icons>
+            <div>
+                <p class="text-whiteDiego">2022</p>
+            </div>
+        </div>
+    </footer>
 </template>
+
+<script>
+import Icons from "../../components/global/Icons.vue";
+export default {
+    components: { Icons },
+    data: () => ({
+        copy: "copyright",
+        web: "web",
+    }),
+};
+</script>

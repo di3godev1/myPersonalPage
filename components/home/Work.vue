206d9cf400e3b0fb094be85ee554bc1a8f9ad905
Diego Reyes <diegorele13@gmail.com>
2022-05-14 09:48:34 -0500

    delete unnecesary space

--- components/home/Work.vue
+++ components/home/Work.vue
@@ -1,5 +1,5 @@
 <template>
-    <section id="workSection " class="max-w-screen-2xl md:mt-36 lg:mt-52">
+    <section id="workSection" class="max-w-screen-2xl md:mt-36 lg:mt-52">
         <div class="w-full">
             <!-- SVG SECTION -->
             <!-- <my-work class="flex sm:hidden pl-5" />

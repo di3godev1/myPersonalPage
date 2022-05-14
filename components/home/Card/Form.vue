3aba5102d55d340b4aaa6c36bca85331b2bd6389
Diego Reyes <diegorele13@gmail.com>
2022-05-14 11:28:04 -0500

    fix: form in some views

--- components/home/Card/Form.vue
+++ components/home/Card/Form.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-full flex justify-center items-center max-w-5xl">
-        <div class="flex flex-col relative top-7 left-4 md:top-48 w-4/5">
-            <div class="relative left-4 md:left-0">
+        <div class="flex flex-col relative top-7 left-2 md:top-48 w-4/5">
+            <div class="flex flex-col md:flex-row">
                 <label for="name" class="text-whiteDiego text-xl md:text-3xl">
                     name:
                 </label>
@@ -9,10 +9,10 @@
                     type="text"
                     placeholder="insert text"
                     id="name"
-                    class="text-whiteDiego w-2/4 bg-blackDiego border md:w-3/5 border-pinkDiego rounded-3xl placeholder:text-sm placeholder:text-greyDiego placeholder:font-s"
+                    class="text-whiteDiego w-full bg-blackDiego border border-pinkDiego rounded-3xl placeholder:text-sm placeholder:text-greyDiego placeholder:font-s"
                 />
             </div>
-            <div class="md:mt-5 flex">
+            <div class="md:mt-5 flex flex-col md:flex-row">
                 <label for="email" class="text-whiteDiego text-xl md:text-3xl">
                     email:
                 </label>
@@ -20,14 +20,14 @@
                     type="text"
                     placeholder="insert text"
                     id="email"
-                    class="text-whiteDiego relative left-3 w-3/5 bg-blackDiego border border-pinkDiego rounded-3xl placeholder:text-sm placeholder:text-greyDiego placeholder:font-secondary"
+                    class="text-whiteDiego w-full bg-blackDiego border border-pinkDiego rounded-3xl placeholder:text-sm placeholder:text-greyDiego placeholder:font-secondary"
                 />
             </div>
-            <div class="mt-2 md:mt-5">
+            <div class="mt-2 md:mt-5 md:flex-row">
                 <textarea
                     type="text"
                     placeholder="insert text"
-                    class="md:w-4/5 mx-2 text-center text-whiteDiego w-5/6 md:h-16 bg-blackDiego border border-pinkDiego rounded-3xl placeholder:text-sm placeholder:text-greyDiego placeholder:font-secondary"
+                    class="text-center text-whiteDiego w-full md:h-16 bg-blackDiego border border-pinkDiego rounded-3xl placeholder:text-sm placeholder:text-greyDiego placeholder:font-secondary"
                 />
             </div>
             <div class="w-full mt-3">

e37c0863743b21e472dffe8d8671a94777f7973a
Diego Reyes <diegorele13@gmail.com>
2022-05-20 17:37:36 -0500

    fix: skills responsive

--- components/home/Skills.vue
+++ components/home/Skills.vue
@@ -12,7 +12,7 @@
                     <my-skills-desktop class="hidden lg:flex" />
                 </div>
                 <!-- SVG TO TABLET -->
-                <div>
+                <div class="">
                     <my-skills-tablet class="hidden sm:flex lg:hidden" />
                 </div>
             </div>
@@ -27,18 +27,18 @@
                     vue-html-css-nuxt-tailwindcss-javascript
                 </p>
             </div>
-            q
+
             <!-- CIRCLE -->
             <div
-                class="w-7/12 h-48 sm:h-64 md:h-96 lg:h-96 border-l-8 border-y-4 relative z-0 border-r-2 border-pinkDiego rounded-full md:w-1/2 size-desktop"
+                class="w-48 h-48 sm:h-80 sm:w-80 md:h-96 lg:h-96 border-l-8 border-y-4 relative z-0 border-r-2 border-pinkDiego rounded-full md:w-1/2 size-desktop size-353px"
             />
             <!-- IMAGE -->
             <div
-                class="w-1/2 md:h-96 size-desktop absolute top-0 md:top left-1/4 right-0 z-20 rounded-full"
+                class="w-1/2 md:h-96 size-desktop size-353px absolute top-0 md:top-1 left-1/4 right-0 z-20 rounded-full left-353px"
             >
                 <img
                     src="~/static/images/ImagesAboutMe/elbichosiu.png"
-                    class="h-48 md:h-96 absolute top-0 left-0 right-0 w-full object-cover object-center mask mask-tablet size-desktop"
+                    class="md:h-96 absolute -top-1 -left-0 right-0 w-full object-cover object-center mask size-desktop size-353px"
                 />
             </div>
         </div>
@@ -50,15 +50,64 @@
     clip-path: circle(50% at 50% 50%);
 }
 
-@media only screen and (min-device-width: 358px) and (max-device-width: 768px) {
-    .mask-tablet {
-        clip-path: circle(48.7% at 49% 50%);
-    }
-}
-
-@media only screen and (min-device-width: 1024px) and (max-device-width: 1440px) {
+@media only screen and (min-device-width: 1024px) and (max-device-width: 1024px) {
     .size-desktop {
         height: 520px;
+    }
+}
+@media only screen and (min-device-width: 353px) and (max-device-width: 639px) {
+    .size-353px {
+        height: 340px;
+        width: 340px;
+    }
+    .left-353px {
+        position: absolute;
+        left: 13px;
+    }
+    .right-353px {
+        position: absolute;
+        left: 0px;
+        margin: 0 auto;
+    }
+}
+@media only screen and (min-device-width: 425px) and (max-device-width: 509px) {
+    .size-353px {
+        height: 340px;
+        width: 340px;
+    }
+    .left-353px {
+        position: absolute;
+        left: 15%;
+    }
+}
+@media only screen and (min-device-width: 510px) and (max-device-width: 700px) {
+    .size-353px {
+        height: 340px;
+        width: 340px;
+    }
+    .left-353px {
+        position: absolute;
+        left: 22%;
+    }
+}
+@media only screen and (min-device-width: 701px) and (max-device-width: 920px) {
+    .size-353px {
+        height: 340px;
+        width: 340px;
+    }
+    .left-353px {
+        position: absolute;
+        left: 30%;
+    }
+}
+@media only screen and (min-device-width: 921px) and (max-device-width: 1024px) {
+    .size-353px {
+        height: 340px;
+        width: 340px;
+    }
+    .left-353px {
+        position: absolute;
+        left: 32%;
     }
 }
 </style>

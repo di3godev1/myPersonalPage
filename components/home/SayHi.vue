39235a6f303143c3d222fd76ea4454b6e96583e9
Diego Reyes <diegorele13@gmail.com>
2022-05-28 09:58:47 -0500

    delete: circle in form

--- components/home/SayHi.vue
+++ components/home/SayHi.vue
@@ -20,23 +20,18 @@
                     <my-say-hi-tablet class="hidden sm:flex lg:hidden" />
                 </div>
             </div>
-            <div
-                class="rounded-full w-72 h-72 border-l-8 border-r-2 border-t-4 border-b-4 border-pinkDiego md:w-96 md:h-96 relative -top-4 lg:left-12 lg:-top-6 md:-top-6 size-tablet"
-            >
-                <HomeCardForm />
-            </div>
-            <div>
-                <Media />
-            </div>
+        </div>
+        <div class="w-full">
+            <HomeCardForm />
         </div>
     </section>
 </template>
 
 <script>
-import Media from "../home/Social/Media.vue";
+// import Media from "../home/Social/Media.vue";
 export default {
     components: {
-        Media,
+        // Media,
     },
 };
 </script>

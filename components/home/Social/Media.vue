4025857586f6c6419b9bd935453208e4d6a349d5
Diego Reyes <diegorele13@gmail.com>
2022-06-01 18:52:50 -0500

    change: personal's links

--- components/home/Social/Media.vue
+++ components/home/Social/Media.vue
@@ -1,20 +1,21 @@
 <template>
-    <div class="w-full">
-        <div
-            class="w-full flex flex-wrap justify-between mx-auto md:relative md:left-3/4"
-        >
-            <div
+    <div class="w-full mt-14">
+        <div class="ml-5 w-5/6 flex justify-start">
+            <h3 class="uppercase text-whiteDiego text-2xl">social media</h3>
+        </div>
+        <div class="flex">
+            <a
                 v-for="(icon, index) in icons"
                 :key="index"
-                class="mt-14 ml-4 w-12 h-12 border-l-2 border-t-4 border-b-4 border-r-8 rounded-full border-pinkDiego flex justify-center items-center"
+                :href="icon.url"
+                target="_blank"
+                class=""
             >
-                <a :href="icon.url" target="_blank">
-                    <Icons
-                        class="w-6 h-6 text-whiteDiego"
-                        :name="icon.name"
-                    ></Icons>
-                </a>
-            </div>
+                <Icons
+                    :name="icon.name"
+                    class="w-6 h-6 text-whiteDiego"
+                ></Icons>
+            </a>
         </div>
     </div>
 </template>
@@ -31,15 +32,15 @@ export default {
             },
             {
                 name: "instagram",
-                url: "https://www.instagram.com/di3godev/",
+                url: "https://www.instagram.com/yeyo.dev/",
             },
             {
                 name: "twitter",
-                url: "https://twitter.com/di3godev",
+                url: "https://twitter.com/yeyodev",
             },
             {
                 name: "github",
-                url: "https://github.com/di3godev1",
+                url: "https://github.com/yeyodev1",
             },
         ],
     }),

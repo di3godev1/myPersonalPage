4756a2dbfb9e02a7fc5e819f2abef819c8bf4828
Diego Reyes <diegorele13@gmail.com>
2022-06-01 19:36:32 -0500

    done: finally doneeeeeee

---
+++ components/home/Social/MediaDesktop.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="w-full mt-24 ml-8">
+        <div class="ml-5 w-5/6 flex justify-start flex-col">
+            <a
+                v-for="(network, index) in networks"
+                :key="index"
+                target="_blank"
+                :href="network.url"
+                class="text-4xl text-whiteDiego mb-4"
+                >{{ network.name }}</a
+            >
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        networks: [
+            {
+                name: "facebook",
+                url: "https://www.facebook.com/di3godev",
+            },
+            {
+                name: "instagram",
+                url: "https://www.instagram.com/yeyo.dev/",
+            },
+            {
+                name: "twitter",
+                url: "https://twitter.com/yeyodev",
+            },
+            {
+                name: "github",
+                url: "https://github.com/yeyodev1",
+            },
+        ],
+    }),
+};
+</script>

4f3497dd224de6ab9b85f473ab31f09c256a58dc
Diego Reyes <diegorele13@gmail.com>
2022-06-01 19:16:53 -0500

    done: social media in mobile views

--- components/home/Social/Media.vue
+++ components/home/Social/Media.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="w-full mt-14">
+    <div class="w-full mt-14 sm:pt-20 mx-auto">
         <div class="ml-5 w-5/6 flex justify-start">
             <h3 class="uppercase text-whiteDiego text-2xl">social media</h3>
         </div>
-        <div class="flex">
+        <div class="flex w-5/6 ml-5 sm:w-2/5">
             <a
                 v-for="(icon, index) in icons"
                 :key="index"
                 :href="icon.url"
                 target="_blank"
-                class=""
+                class="w-12 h-12 mt-5 mr-5 border-l border-t-2 border-b-2 border-r-4 rounded-full border-pinkDiego flex items-center justify-center"
             >
                 <Icons
                     :name="icon.name"

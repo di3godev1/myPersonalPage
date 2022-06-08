2c30b09b53df08d34efcb53426e1cc9e63cef3ad
Diego Reyes <diegorele13@gmail.com>
2022-06-07 19:43:55 -0500

    fix: font

--- components/The/Works.vue
+++ components/The/Works.vue
@@ -12,7 +12,9 @@
                     </p>
                 </div>
                 <div class="row-start-3 col-end-4 col-start-1 relative -top-3">
-                    <p class="text-whiteDiego text-xs sm:text-base lg:text-2xl">
+                    <p
+                        class="text-whiteDiego text-xs sm:text-base lg:text-2xl font-secondary"
+                    >
                         {{ description }}
                     </p>
                 </div>

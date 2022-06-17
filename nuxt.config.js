5ba0da043ff3f0e7d92a345d04d98ad70f73aa7b
Diego Reyes <diegorele13@gmail.com>
2022-06-17 15:18:16 -0500

    change: new head

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
-        title: "personalPage",
+        title: "yeyodev",
         meta: [
             { charset: "utf-8" },
             {
